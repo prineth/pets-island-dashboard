27a0f88d5fa5955637ec2fb0392832e49d4155d3
prineth <prinethfernandox@gmail.com>
2022-06-04 14:20:55 +0530

    update to prineth firebase

--- src/firebase-confic.js
+++ src/firebase-confic.js
@@ -1,18 +1,22 @@
-import { initializeApp } from "firebase/app";
 import {getFirestore} from '@firebase/firestore'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDuqkzvjR-WRqRjexadVePl5THVz2YZf60",
-    authDomain: "mobile-uni.firebaseapp.com",
-    projectId: "mobile-uni",
-    storageBucket: "mobile-uni.appspot.com",
-    messagingSenderId: "601334681686",
-    appId: "1:601334681686:web:d483002bb3dd4e25622bf7",
-    measurementId: "G-7KH6QYTYLF"
-  };
+  apiKey: "AIzaSyAVUUTJW7YqxXRpqBxotE9PS4EPgDrmchM",
+  authDomain: "pets-island.firebaseapp.com",
+  databaseURL: "https://pets-island-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "pets-island",
+  storageBucket: "pets-island.appspot.com",
+  messagingSenderId: "184876335281",
+  appId: "1:184876335281:web:5ae9423170ac2794e1e290"
+};
 
-
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
